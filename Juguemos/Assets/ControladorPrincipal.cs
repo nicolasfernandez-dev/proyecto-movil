4b0015c767cbf9b3b9e0f5074924d7a0ab3b1073
Nico <niicofdz@gmail.com>
2026-04-30 13:07:35 +0200

    feat: implementar control táctil bidireccional y muros físicos laterales

--- Juguemos/Assets/ControladorPrincipal.cs
+++ Juguemos/Assets/ControladorPrincipal.cs
@@ -2,18 +2,34 @@ using UnityEngine;
 
 public class ControladorPrincipal : MonoBehaviour
 {
-public float velocidad = 5f;
+    public float velocidad = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        if(Input.GetMouseButton(0))
+        if(Input.GetMouseButton(0)) //el 0 indica el click izquierdo, 1 = click derecho, 2 = boton central(rueda ratón)
         {
-            transform.Translate(Vector3.right * velocidad * Time.deltaTime);
+            float mitadPantalla = Screen.width / 2f;
+
+            if(Input.mousePosition.x > mitadPantalla){
+            rb.linearVelocity = new Vector2(velocidad, rb.linearVelocity.y);
+            }
+
+            else
+            {
+                rb.linearVelocity = new Vector2(-velocidad, rb.linearVelocity.y);
+            }
         }
 
         else
         {
-            //el cuadrado se queda quieto
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y); //el cuadrado se queda quieto
         }
     }
 }
