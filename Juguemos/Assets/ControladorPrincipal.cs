d153552b60f912cc47a08e6279a30b04705d3673
Nico <niicofdz@gmail.com>
2026-05-04 08:35:46 +0200

    feat: primer modelo de minijuego, sin pinchos. Rebote de paredes y  dirección única

--- Juguemos/Assets/ControladorPrincipal.cs
+++ Juguemos/Assets/ControladorPrincipal.cs
@@ -9,24 +9,36 @@ public class ControladorPrincipal : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool estabaPulsando = false;
+    private bool juegoIniciado = false;
+    private float direccionX = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f;
     }
 
     void Update()
     {
-        bool pulsandoAhora = Input.GetMouseButton(0);
+        bool pulsandoAhora = Input.GetMouseButton(0); //el 0 indica el click izquierdo, 1 = click derecho, 2 = boton central(rueda ratón)
 
-        if(pulsandoAhora && !estabaPulsando) //el 0 indica el click izquierdo, 1 = click derecho, 2 = boton central(rueda ratón)
+        if(!juegoIniciado && pulsandoAhora && !estabaPulsando) 
         {
+            rb.gravityScale = 1f;
             float mitadPantalla = Screen.width / 2f;
-            float direccionX = Input.mousePosition.x > mitadPantalla ? 1f : -1f; //variable = condicion ? valor_si_true : valor_si_false
+            direccionX = Input.mousePosition.x > mitadPantalla ? 1f : -1f; //variable = condicion ? valor_si_true : valor_si_false
+            juegoIniciado = true;
+        }
 
+        if (juegoIniciado)
+        {
+            rb.linearVelocity = new Vector2(direccionX * velocidad, rb.linearVelocity.y);
+        }
+
+        if (juegoIniciado && pulsandoAhora && !estabaPulsando)
+        {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-
-            rb.AddForce(new Vector2(direccionX * velocidad, fuerzaSalto), ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
         }
 
         estabaPulsando = pulsandoAhora;
@@ -37,6 +49,11 @@ public class ControladorPrincipal : MonoBehaviour
         if (collision.gameObject.CompareTag("LimiteMuerte"))
         {
             morir();
+        }
+
+        if (collision.gameObject.CompareTag("LimiteLateral"))
+        {
+            direccionX *= -1f;
         }
     }
 
